03765408d56d2a862edd0d18c1ecce6fc3babcc6
Hendrix <lentsidjahendrix10@gmail.com>
2026-03-22 20:33:42 -0400

    Livrable 2 - JavaFX CRUD Repository JSON - version finale

--- Fil-rouge/src/main/java/com/filrouge/model/YouTubeVideo.java
+++ Fil-rouge/src/main/java/com/filrouge/model/YouTubeVideo.java
@@ -2,7 +2,7 @@ package com.filrouge.model;
 import java.time.LocalDateTime;
 
 public class YouTubeVideo extends PublicationMediaSociale {
-    private String qualite; // ex: "1080p", "4K"
+    private String qualite;
     private boolean monetisee;
 
     public YouTubeVideo(int id, String auteur, LocalDateTime date, String url, int duree, String qualite, boolean monetisee) {
@@ -11,18 +11,20 @@ public class YouTubeVideo extends PublicationMediaSociale {
         this.monetisee = monetisee;
     }
 
+    public YouTubeVideo() { super(); }
+
     @Override
     public boolean isValid() {
-        // Règle Niveau 3 : Une vidéo YouTube doit durer au moins 30 secondes
         return super.isValid() && getDureeSecondes() >= 30;
     }
-    
-    public YouTubeVideo() {
-        super(); // Appelle le constructeur vide de Publication
+
+    @Override
+    public String toInfo() {
+        return super.toInfo() + "\nQualité: " + qualite + "\nMonétisée: " + monetisee;
     }
-    
-    public String getQualite()    { return qualite;    }
-    public boolean isMonetisee()  { return monetisee;  }
-        }
-    
-    // Pour corriger proprement, assure-toi d'ajouter un getter public int getDureeSecondes() dans PublicationMediaSociale.
+
+    public String getQualite()          { return qualite;    }
+    public boolean isMonetisee()        { return monetisee;  }
+    public void setQualite(String q)    { this.qualite = q;  }
+    public void setMonetisee(boolean m) { this.monetisee = m; }
+}
